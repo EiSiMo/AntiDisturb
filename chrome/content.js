e9f131047dc427bb5010d69ee6fb0c6924398e59
Moritz Eigenauer <moritz.eigenauer@gmail.com>
2022-04-02 19:37:48 +0200

    Added basic unstable unblur

--- chrome/content.js
+++ chrome/content.js
@@ -1,3 +1,6 @@
+const fullBlur = 10;
+const unblurStep = 7;
+
 // Returns true if it is a DOM element
 // credit https://stackoverflow.com/a/384380
 function isElement(o){
@@ -7,24 +10,53 @@ function isElement(o){
 );
 }
 
+function mediaClicked() {
+    blur = getElementBlur(this.style["filter"]);
+    if (blur == 0) {
+        this.style["filter"] = `blur(${fullBlur}px)`;
+    } else if (blur - unblurStep >= 0) {
+        this.style["filter"] = `blur(${blur - unblurStep}px)`;
+    } else {
+        this.style["filter"] = `blur(${0}px)`;
+    }
+}
+
+function getElementBlur(filter) {
+    // regex to find the blur amount in px from the css filter argument
+    const regexBlur = /.*blur\((\d+)px\).*/;
+    const match = filter.match(regexBlur);
+    if (match) {
+        return parseInt(match[1]);
+    } else {
+        return 0;
+    }
+    
+}
+
 function checkMutations(mutations) {
     for (mutation of mutations) {
         for (node of mutation.addedNodes) {
             if (isElement(node) == true) {
+                // images
                 let images = node.getElementsByTagName("img");
                 for (elt of images) {
-                    elt.style["filter"] = "blur(8px)";
+                    elt.style["filter"] = `blur(${fullBlur}px)`;
+                    elt.onclick = mediaClicked;
                 }
 
+                // divs
                 let videos = document.getElementsByTagName("video");
                 for (elt of videos) {
-                    elt.style["filter"] = "blur(8px)";
+                    elt.style["filter"] = `blur(${fullBlur}px)`;
+                    elt.onclick = mediaClicked;
                 }
 
+                // divs with a background image in css
                 let divs = document.getElementsByTagName("div");
                 for (elt of divs) {
                     if (elt.style.backgroundImage) {
-                        elt.style["filter"] = "blur(8px)";
+                        elt.style["filter"] = `blur(${fullBlur}px)`;
+                        elt.onclick = mediaClicked;
                     }
                 }
             }
@@ -35,4 +67,3 @@ function checkMutations(mutations) {
 var observer = new MutationObserver(checkMutations);
 
 observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
-
